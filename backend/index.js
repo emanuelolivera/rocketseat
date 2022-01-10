99923a8db0c79be10300b02e09fe35feda33ab3d
emanueloliveira <marquesomanel@gmail.com>
2022-01-10 01:34:43 -0300

    .

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response)=>{})
+app.use(express.json())
+
+app.post('/users', (request, response)=>{
+    return response.json({
+    evento:'aula',
+    aluno:'emanuel'
+})
+})
 
 app.listen(3333);
