01c2c81afa4b43587a2288495539fc4d815a2949
emanueloliveira <marquesomanel@gmail.com>
2022-01-10 23:26:30 -0300

    rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const crypto = require('crypto')
 
 const routes = express.Router();
 
 routes.post('/ongs', (request, response) =>{
     const {nome, email, whatsapp, city, uf } = request.body;
-
+    const id = crypto.randomBytes(4).toString('HEX')/*usado o metodo crypto para gerar um numero aleatorio e passando para string como hexadecimal */
 
     return response.json();
 })
