e8934b9a6e8a3232fe91b8c8aa74d8430429dcf6
emanueloliveira <marquesomanel@gmail.com>
2022-01-11 01:36:02 -0300

    rotas

--- backend/src/database/migrations/20220111013309_create_incidents.js
+++ backend/src/database/migrations/20220111013309_create_incidents.js
@@ -7,9 +7,9 @@ exports.up = function(knex) {
     table.string('description').notNullable();
     table.decimal('value').notNullable();
 
-    table.string('ongs_id').notNullable();
+    table.string('ong_id').notNullable();
 
-    table.foreign('ongs_id').references('id').inTable('ongs')
+    table.foreign('ong_id').references('id').inTable('ongs')
     /* chave estrageira referenciando o id da tabela ong */ 
 
   })
