2fad959dad59edc19c19c884f55556bdc0a913a0
emanueloliveira <marquesomanel@gmail.com>
2022-01-10 22:42:42 -0300

    fazendo as tabelas

---
+++ backend/src/database/migrations/20220111013309_create_incidents.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('incidents', function(table){
+    table.increments();
+    
+    table.string('title').notNullable();
+    table.string('description').notNullable();
+    table.decimal('value').notNullable();
+
+    table.string('ong_id').notNullable();
+
+    table.foreign('ongs_id').references('id').table('ongs')
+    /* chave estrageira referenciando o id da tabela ong */ 
+
+  })
+};
+
+exports.down = function(knex) {
+  
+};
