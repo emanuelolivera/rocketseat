b533d9f8f5d179f5190707e752c078d46082fc8f
emanueloliveira <marquesomanel@gmail.com>
2022-01-11 00:09:29 -0300

    data

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const crypto = require('crypto')
+const connection = require('./database/connection')
 
 const routes = express.Router();
 
-routes.post('/ongs', (request, response) =>{
-    const {nome, email, whatsapp, city, uf } = request.body;
-    const id = crypto.randomBytes(4).toString('HEX')/*usado o metodo crypto para gerar um numero aleatorio e passando para string como hexadecimal */
+routes.post('/ongs', async (request, response) =>{
+    const {name, email, whatsapp, city, uf } = request.body;
+    const id = crypto.randomBytes(4).toString('HEX');/*metodo crypto para gerar um numero e passando para string como hexadecimal */
 
-    return response.json();
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
+    })
+
+    return response.json({id});
 })
 module.exports = routes;
